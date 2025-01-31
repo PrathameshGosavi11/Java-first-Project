518a82c3cd02238d27fc2c0108bdb4b6a469aaca
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-01 01:11:52 +0530

    add new multiplication feature

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,12 @@ class Calci
 		int sub=a-b;
 		System.out.println("Sustraction is:"+sub);
 	}
+
+	public void mul(int a,int b)
+	{
+		return mult=a*b;
+		System.out.println("multiplication is :"+mult);
+	}
 }
 public class Calculator {
 	
@@ -13,5 +19,6 @@ public static void main(String[] args) {
 		
 		Calci calci=new Calci();
 		calci.substract(3,1);
+		calci.mul(2,2);
 	}
 }
