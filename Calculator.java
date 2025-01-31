2afa40d03acb5991b085c087a289e68c50b4be8c
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-01 00:58:29 +0530

    added substract functionality

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,12 @@ class Calci
 		int sum=a+b;
 		System.out.println("sum is:"+sum);
 	}
+	
+	public void substract(int a,int b)
+	{
+		int sub=a-b;
+		System.out.println("Sustraction is:"+sub);
+	}
 }
 public class Calculator {
 	
@@ -12,5 +18,6 @@ public static void main(String[] args) {
 		
 		Calci calci=new Calci();
 		calci.add(2, 2);
+		calci.substract(3,1);
 	}
 }
