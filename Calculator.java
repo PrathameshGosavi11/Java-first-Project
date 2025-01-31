5bfaeeeda98b14a498ef9344cfe7320163c0bd9f
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-01 01:05:26 +0530

    remove the additon feature

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,6 @@
 class Calci
 {
-	public void  add(int a,int b)
-	{
-		int sum=a+b;
-		System.out.println("sum is:"+sum);
-	}
-	
+		
 	public void substract(int a,int b)
 	{
 		int sub=a-b;
@@ -17,7 +12,6 @@ public class Calculator {
 public static void main(String[] args) {
 		
 		Calci calci=new Calci();
-		calci.add(2, 2);
 		calci.substract(3,1);
 	}
 }
